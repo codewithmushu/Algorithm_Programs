ad84874945d8bac24e0a6fdec65cdb00cc2634ef
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 23:49:33 +0530

    [Muskan] Added : static functions to return all permutations ofa String using iterative method and Recursion method.

---
+++ BinaryTree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public Node Root { get; set;}
+
+        public bool Add(int Value)
+        {
+            Node before = null, after = this.Root;
+            
+            while(after != null)
+            {
+                before = after;
+                if (Value < after.Data)
+                {
+                    after = after.LeftNode;
+                }
+                else if(Value > after.Data)
+                {
+                    after = after.RightNode;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            Node newnode = new Node();
+            newnode.Data = Value;
+            if(this.Root == null)
+            {
+                this.Root = newnode;
+            }
+            else
+            {
+                if (Value<before.Data)
+                {
+                    before.LeftNode = newnode;
+                }
+                else
+                {
+                    before.RightNode = newnode;
+                }
+            }
+            return true;
+        }
+
+
+        public void TravarsePreorder(Node Parent)
+        {
+            if (Parent != null)
+            {
+                Console.WriteLine(Parent.Data + " ");
+                TravarsePreorder(Parent.LeftNode);
+                TravarsePreorder(Parent.RightNode);
+            }
+        }
+
+
+
+
+    }
+}
