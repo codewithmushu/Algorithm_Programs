7f120755a9d3c2e5b0f353d6b4ede2d2164dbddc
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-03-28 23:42:49 +0530

    [Muskan] Added : Main method Data structure and algorithm problems.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+
+            //binaryTree.Add(50);
+            //binaryTree.Add(20);
+            //binaryTree.Add(30);
+            //binaryTree.Add(15);
+            //binaryTree.Add(60);
+            //binaryTree.Add(80);
+
+
+            binaryTree.TravarsePreorder(binaryTree.Root);
+
+            //InsertionSort sort = new InsertionSort();
+
+            //sort.DisplayInsertion();
+        }
+    }
+}
